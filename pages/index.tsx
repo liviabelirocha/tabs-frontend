f41f1803eb0e2051e5ca03022cb344a87f6b74df
Lívia Belizário <liviabelirocha@outlook.com>
2021-03-15 11:28:42 -0300

    creating project with nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      z
+      <Head>
+        <title>Wheat Tabs</title>
+        <link rel="icon" href="/icon.svg" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
